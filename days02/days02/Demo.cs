aa7fc2d91f59cfe64e86678014e3f5694e1d60af
Nitish Kumar <103942404+nitish906@users.noreply.github.com>
2023-07-07 12:24:51 +0530

    days_02

---
+++ days02/days02/Demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Print "Masai School" in the console followed
+ * by "A Transformation in Education" in next line
+ */
+public class Demo 
+{
+    public static void findString(String str,String str1)
+    {
+        Console.WriteLine(str);
+        Console.WriteLine(str1);
+    }
+
+    public static void Main(string[] args)
+    {
+        String str = "Masai School";
+        String str1 = "A Transformation in Education";
+        findString(str, str1);
+    }
+}
